fbe9cdf5fe9361b7662317b6d2cbb594961c8ed3
Jonah <jrmeoak@gmail.com>
2026-05-03 16:53:57 -0400

    Added untest v1 of half template function

--- Template Assignment/Template Assignment.cpp
+++ Template Assignment/Template Assignment.cpp
@@ -1,15 +1,28 @@
 // Jonah Meoak
 // CIS 1202 M01
 // May 3 2026
-// Character offset program demo thing with exception handling
+// 
 
 #include <iostream>
 
 using namespace std;
+
+template <typename T>
+T half(T value);
 
 int main()
 {
     
 }
 
+template <typename T>
+T half(T value)
+{
+	return value / 2.0;
+}
 
+template <typename int>
+int half(int value)
+{
+	return ++value / 2;
+}
