a5171464590096e2fa462857689326a547541a28
Jonah <jrmeoak@gmail.com>
2026-05-03 16:48:45 -0400

    Add project files.

---
+++ Template Assignment/Template Assignment.cpp
@@ -0,0 +1,15 @@
+// Jonah Meoak
+// CIS 1202 M01
+// May 3 2026
+// Character offset program demo thing with exception handling
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
+
+
