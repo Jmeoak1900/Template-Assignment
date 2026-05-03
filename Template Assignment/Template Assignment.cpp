0198f478be057772fda07edda19fad85bb5daab7
Jonah <jrmeoak@gmail.com>
2026-05-03 16:57:58 -0400

    added tests and fixed half (int version)

--- Template Assignment/Template Assignment.cpp
+++ Template Assignment/Template Assignment.cpp
@@ -10,9 +10,24 @@ using namespace std;
 template <typename T>
 T half(T value);
 
+template <>
+int half<int>(int value);
+
 int main()
 {
-    
+	int i = 5;
+	int i2 = -7;
+	int i3 = 10;
+	double d = 9.0;
+	double d2 = -7.0;
+	float f = 9.0f;
+
+	cout << "int i: " << half(i) << endl;
+	cout << "int i2: " << half(i2) << endl;
+	cout << "int i3: " << half(i3) << endl;
+	cout << "double d: " << half(d) << endl;
+	cout << "double d2: " << half(d2) << endl;
+	cout << "float f: " << half(f) << endl;
 }
 
 template <typename T>
@@ -21,8 +36,8 @@ T half(T value)
 	return value / 2.0;
 }
 
-template <typename int>
-int half(int value)
+template <>
+int half<int>(int value)
 {
-	return ++value / 2;
+	return (value + 1) / 2;
 }
